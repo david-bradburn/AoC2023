ec8079ceca6273dbaf5b598486a3a4e4e6058f7f
David Bradburn <david.bradburn2@gmail.com>
2023-12-02 09:29:19 +0000

    aoc d1 p1

--- Day 1/1/1_1.py
+++ Day 1/1/1_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/1 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "1"
@@ -11,3 +11,29 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+print(rawInput)
+cleanerInput = []
+for i in rawInput:
+	cleanerInput.append(i.strip('\n'))
+
+print(cleanerInput)
+
+sum = 0
+for line in cleanerInput:
+	for char in line:
+		if char.isdigit():
+			firstDigit = char
+			break
+	
+	for char in line[::-1]:
+		if char.isdigit():
+			lastDigit = char
+			break
+	
+	val =  firstDigit + lastDigit
+	val = int(val)
+	print(val)
+	sum += val
+
+print(sum)
