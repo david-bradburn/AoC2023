062b7d50ce577fec74ff05b14d45ddd60433aefe
David Bradburn <david.bradburn2@gmail.com>
2023-12-07 19:23:11 +0000

    d2 p1 and p2, p cool

--- Day 2/1/2_1.py
+++ Day 2/1/2_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/2 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "2"
 PART = "1"
@@ -13,9 +13,65 @@ with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
 
 cleanerInput = [i.strip("\n") for i in rawInput]
-print(cleanerInput)
+# print(cleanerInput)
 
 class ColourGame():
   def __init__(self, input) -> None:
     self.input = input
     self.maxColours_dict = {"r": 12, "g": 13, "b": 14}
+    self.gamedict = {}
+    self.sumofpossiblegames = 0
+    self.processInput()
+
+    print(self.sumofpossiblegames)
+
+  def processInput(self):
+    cleaner = [i.split(":") for i in self.input]
+    # print(cleaner)
+    for game in cleaner:
+      turns = game[1].split(";")
+      match game[0].split(" "):
+        case ["Game", gameID]:
+          game_ID = int(gameID)
+      
+
+      impossibleFlag = False
+      for turn in turns:
+        temp = turn.split(",")
+        # print(temp)
+
+        for ball in temp:
+          clean_balls = ball.strip()
+
+          match clean_balls.split(" "):
+            case [number_of_red_balls, "red"]:
+              red = int(number_of_red_balls)
+              if red > self.maxColours_dict["r"]:
+                 impossibleFlag = True
+            case [number_of_green_balls, "green"]:
+              green = int(number_of_green_balls)
+              if green > self.maxColours_dict["g"]:
+                 impossibleFlag = True
+            case [number_of_blue_balls, "blue"]:
+              blue = int(number_of_blue_balls)
+              if blue > self.maxColours_dict["b"]:
+                 impossibleFlag = True
+            case _:
+                raise Exception
+            
+          # print(clean_balls)
+
+        if impossibleFlag:
+          break
+      if impossibleFlag:
+          continue
+      
+      self.sumofpossiblegames += game_ID
+      print(game_ID)
+      
+
+      
+      
+    # gameStr, turns = self
+
+p1 = ColourGame(cleanerInput)
--- Day 2/2/2_2.py
+++ Day 2/2/2_2.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/2 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "2"
 PART = "2"
@@ -11,3 +11,61 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+cleanerInput = [i.strip("\n") for i in rawInput]
+# print(cleanerInput)
+
+class ColourGame():
+  def __init__(self, input) -> None:
+    self.input = input
+    # self.maxColours_dict = {"r": 12, "g": 13, "b": 14}
+    self.gamedict = {}
+    self.sumofpossiblegames = 0
+    self.processInput()
+
+    print(self.sumofpossiblegames)
+
+  def processInput(self):
+    cleaner = [i.split(":") for i in self.input]
+    # print(cleaner)
+    for game in cleaner:
+      maxballs = {"r": 0, "g": 0, "b": 0 }
+      
+      turns = game[1].split(";")
+      for turn in turns:
+        temp = turn.split(",")
+
+        for ball in temp:
+          clean_balls = ball.strip()
+
+          match clean_balls.split(" "):
+             
+            case [number_of_red_balls, "red"]:
+              red = int(number_of_red_balls)
+              if red > maxballs["r"]:
+                maxballs["r"] = red
+                 
+            case [number_of_green_balls, "green"]:
+              green = int(number_of_green_balls)
+              if green > maxballs["g"]:
+                maxballs["g"] = green
+                 
+            case [number_of_blue_balls, "blue"]:
+              blue = int(number_of_blue_balls)
+              if blue > maxballs["b"]:
+                maxballs["b"] = blue
+            case _:
+                raise Exception
+            
+          # print(clean_balls)
+
+      productofballs = maxballs["r"] * maxballs["g"] * maxballs["b"]
+      self.sumofpossiblegames += productofballs
+    #   print(game_ID)
+      
+
+      
+      
+    # gameStr, turns = self
+
+p1 = ColourGame(cleanerInput)
